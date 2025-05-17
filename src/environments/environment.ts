b1851c4a3c8fb453b72d84a81e01f53b4febe81b
Sergio <u20221d401@upc.edu.pe>
2025-05-17 17:29:39 -0500

    Primer commit desde carpeta local

---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {production: true,
+                            //serverBasePath: "https://my-json-server.typicode.com/estefanojaque/Managewise-reportes"
+                            serverBasePath: "managewise-ffbua6fpfmbteaeq.centralus-01.azurewebsites.net"
+                            //serverBasePath: "http://localhost:3000"
+                            };
